2a036c347c782e768d4cc55db0eca2fbd8e153ab
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2022-11-24 21:25:01 +0200

    2.01 started, not finished

---
+++ part 2/2.01/logoutput/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import cors from "cors";
+import fetch from "node-fetch";
+
+const app = express();
+const port = 3001;
+
+const randomUuid = uuidv4();
+
+const generateUuid = () => {
+  console.log(`${new Date()}:`, randomUuid);
+
+  setTimeout(() => {
+    generateUuid();
+  }, 5000);
+};
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/status", async (req, res) => {
+  const pingpongCount = await fetch("http://pingpong-service:3002");
+  res.send(`${new Date()}: ${randomUuid}\nPing / Pongs: ${pingpongCount}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+generateUuid();
---
+++ part 2/2.01/pingpong/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3002;
+
+app.use(express.json());
+app.use(cors());
+
+let pingpongCount = 0;
+
+app.get("/pingpong", async (req, res) => {
+  res.send(pingpongCount);
+  pingpongCount++;
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
